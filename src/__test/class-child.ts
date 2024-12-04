d371e5781fb348f013e27f8ec63c8f85e6979a11
Chesney Nyffels <chesney@nyffels.be>
2024-12-04 15:50:02 +0100

    Updated test scenarios

--- src/__test/class-child.ts
+++ src/__test/class-child.ts
@@ -12,6 +12,19 @@ export class TestChild {
   public guid: string = "";
 
   @column("test_par_child_test_par_guid")
+  @type("guid")
   @foreignKey<Test1>(Test1, "guid", ForeignKeyOption.Cascade, ForeignKeyOption.SetNull)
   public parentGuid: string = "";
+
+  @column("test_par_child_rd")
+  @type('bigstring')
+  public rawData: string = "";
+
+  @column("test_par_child_money")
+  @type('number', "10.2")
+  public money: number = 0;
+
+  @column("test_par_child_microid")
+  @type("number", "10")
+  public mId: number = 0;
 }
